19e7208cffec5a8d15f1fe110d3c186ef5398c8f
Michał Korecki <korecki.m@gmail.com>
2017-06-20 14:51:05 +0200

    Initial commit.

---
+++ source/Incrementer.Tests/GitTests.fs
@@ -0,0 +1,55 @@
+﻿module Incrementer.Tests.GitTests
+
+open Fake
+open FsUnit
+open NUnit.Framework
+open System
+
+type FakeVersion = Incrementer.Git.Version
+
+
+let createMessages commits = fun _ ->
+    let consoleMessages =
+        commits
+        |> Seq.map (fun c -> { IsError = false; Message = c; Timestamp = DateTimeOffset(DateTime.Now); })
+    (true, consoleMessages)
+
+let createParameters parameters =
+    parameters
+
+let shouldEqual (expected : FakeVersion) (actual : FakeVersion) =
+    actual.Major |> should equal expected.Major
+    actual.Minor |> should equal expected.Minor
+    actual.Patch |> should equal expected.Patch
+
+[<Test>]
+let ``Version is equal to number of commits when there is no tag in repo commit history`` () =
+    let getMessages =
+        createMessages [
+            "44d1fca (HEAD -> master) Setup FAKE and build script.";
+            "1e90a42 (origin/master, origin/HEAD) Add linqpad POC.";
+            "ca3cf48 Initial commit";
+        ]
+
+    let version = Incrementer.Git.getRepositoryVersionUsingProcess createParameters getMessages
+
+    version |> shouldEqual { Major = 1; Minor = 0; Patch = 3; }
+
+[<Test>]
+let ``Version is equal to number of commits after most recent tag`` () =
+    let getMessages =
+        createMessages [
+            "4510e19 Adjust package restore.";
+            "be419bf Remove process-killing from build.";
+            "67d55d6 Adjust binaries names.";
+            "9dc43df Fix config paths.";
+            "da24eba (tag: v1.2) Add more clear status messages, notify when";
+            "87e123c Adjust build to include fsharp projects.";
+            "aa1ccd3 Merge branch FSharp. Resolves #37, resolves #40.";
+            "0a4bef9 Fix a bug causing program to crash when existing ticker";
+            "d85d467 (tag: v1.1) Adjust build scripts for SQLite deployment.";
+        ]
+
+    let version = Incrementer.Git.getRepositoryVersionUsingProcess createParameters getMessages
+
+    version |> shouldEqual { Major = 1; Minor = 2; Patch = 4; }
---
+++ source/Incrementer/Git.fs
@@ -0,0 +1,57 @@
+﻿module Incrementer.Git
+
+open Fake
+open ProcessHelper
+open System
+open System.Text.RegularExpressions
+
+// Tests
+// several tags
+// no tags
+// no commits
+// execproc fail
+
+type Parameters = {
+    GitExecutablePath: string;
+    GitRepositoryPath: string;
+    Branch: string;
+}
+
+type Version = {
+    Major: int;
+    Minor: int;
+    Patch: int;
+}
+
+let getRepositoryVersionUsingProcess (changeParameters : Parameters -> Parameters) (execProcess : Parameters -> bool * seq<ConsoleMessage>) =
+    let defaultParameters = { GitExecutablePath = "git.exe"; Branch = "master"; GitRepositoryPath = "." }
+    let parameters = changeParameters defaultParameters
+    let status, messages = execProcess parameters
+    let extractVersionByTag (index : int) (consoleMessage : ConsoleMessage) =
+        let matched = Regex.Match(consoleMessage.Message, @"\(tag: [vV]?(?<major>[0-9]+)\.?(?<minor>[0-9]+)?\)")
+        if matched.Success then
+            let minorGroup = matched.Groups.["minor"]
+            let minor = if minorGroup.Success then Int32.Parse(minorGroup.Value) else 0
+            let major = Int32.Parse(matched.Groups.["major"].Value)
+            Some { Major = major; Minor = minor; Patch = index; }
+        else
+            None
+            
+    let mostRecentTagVersion =
+        messages
+        |> Seq.mapi extractVersionByTag
+        |> Seq.choose id
+        |> Seq.tryHead
+        
+    match mostRecentTagVersion with
+    | Some(v) -> v
+    | _ -> { Major = 1; Minor = 0; Patch = (Seq.length messages); }
+
+let getRepositoryVersion changeParameters =
+    let execGitProcess =
+        fun parameters -> ExecProcessRedirected (fun info ->
+            info.FileName <- parameters.GitExecutablePath
+            info.WorkingDirectory <- parameters.GitRepositoryPath
+            info.Arguments <- sprintf "log %s --oneline --decorate" parameters.Branch
+            info.CreateNoWindow <- true) (TimeSpan.FromSeconds 30.0)
+    getRepositoryVersionUsingProcess changeParameters execGitProcess
