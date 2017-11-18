debd54b24fff8802dc993e11648d49e718470deb
Michał Korecki <korecki.m@gmail.com>
2017-11-18 18:54:39 +0100

    Include publish target in build script.

--- build/build.fsx
+++ build/build.fsx
@@ -14,15 +14,6 @@ let deployables = (!! "./source/**/*.?sproj") -- "./source/**/*Test*.?sproj"
 let testables = !! "./source/**/*Test*.?sproj"
 
 
-Target "RunTests" (fun _ ->
-    !! (testDirectory + "/*Tests.dll")
-        |> NUnit3 (fun nunitParams ->
-            { nunitParams with
-                ShadowCopy = false;
-                ToolPath = "build\\tools\\NUnit.ConsoleRunner\\tools\\nunit3-console.exe";
-                ResultSpecs = [ testDirectory + "/TestsResults.xml" ] })
-)
-
 Target "Clean" (fun _ ->
     CleanDirs [outputDirectory;testDirectory]
 )
@@ -33,10 +24,42 @@ Target "Compile" (fun _ ->
     MSBuildRelease testDirectory "Build" testables |> Log "Compile: "
 )
 
+Target "RunTests" (fun _ ->
+    !! (testDirectory + "/*Tests.dll")
+        |> NUnit3 (fun nunitParams ->
+            { nunitParams with
+                ShadowCopy = false;
+                ToolPath = "./build/tools/NUnit.ConsoleRunner/tools/nunit3-console.exe";
+                ResultSpecs = [ testDirectory + "/TestsResults.xml" ] })
+)
+
+Target "Publish" (fun _ ->
+    let key = getBuildParam "key"
+    let version = getBuildParam "version"
+    NuGetPack 
+        (fun p ->
+           { p with
+               Version = version
+               OutputPath = outputDirectory
+               WorkingDir = outputDirectory
+               ToolPath = "./build/nuget.exe" })
+        "./source/Incrementer/Incrementer.fsproj"
+    NuGetPublish
+        (fun p ->
+            { p with
+               AccessKey = key
+               Version = version
+               OutputPath = outputDirectory
+               WorkingDir = outputDirectory
+               ToolPath = "./build/nuget.exe"
+               Project = "Incrementer" })
+)
+
 
 "Clean"
   ==> "Compile"
   ==> "RunTests"
+  ==> "Publish"
 
 
 RunTargetOrDefault "RunTests"
